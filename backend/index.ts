80a94eef963c3543c0d461e67b1bf4c33dec674c
carmanolo <carlos.dominguez2301@alumnos.ubiobio.cl>
2026-08-28 21:12:46 -0400

    creación de index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,40 @@
-import express, { Request, Response } from 'express';
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+import indexRoutes from "./src/routes/index.routes.js";
+import { PORT, HOST } from "./src/config/configEnv.js";
+import { connectDB } from "./src/config/configDb.js";
+import { createUsers } from "./src/config/initDb.js";
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+async function setupServer(): Promise<void> {
+  const app = express();
+  app.disable("x-powered-by");
 
-// Middleware para entender JSON
-app.use(express.json());
+  app.use(
+    cors({
+      credentials: true,
+      origin: true,
+    })
+  );
 
-// Ruta de prueba (con tipado explícito de Request y Response)
-app.get('/', (req: Request, res: Response) => {
-    res.json({ message: '¡Bienvenido a mi API con Express y TypeScript!' });
-});
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use("/api", indexRoutes);
+  app.listen(Number(PORT), () => {
+    console.log(`Servidor corriendo en ${HOST}:${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+async function setupAPI(): Promise<void> {
+  try {
+    await connectDB();
+    await createUsers();
+    await setupServer();
+  } catch (error) {
+    console.error("Error en index.ts -> setupAPI(): ", error);
+  }
+}
+
+setupAPI()
+  .then(() => console.log("=> API Iniciada exitosamente"))
+  .catch((error) => console.log("Error en index.ts -> setupAPI(): ", error));
